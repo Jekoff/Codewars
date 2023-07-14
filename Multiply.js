6241a437665c9fe2644b895f9b39fe4ea310cee7
“Jekoff” <jlanloze@gmail.com>
2023-07-14 04:13:35 +0200

    New challenge

---
+++ Multiply.js
@@ -0,0 +1,16 @@
+// This code does not execute properly. Try to figure out why.
+/* 
+function multiply(a, b){
+    a * b
+}
+*/
+
+//My solution
+
+function multiply(a, b){
+  return a * b
+ }
+
+// Another solutions
+
+multiply = (a, b) => a * b;
