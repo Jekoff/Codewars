4e4cfa77bcd236d01518755cec723673dd0b1344
Jekoff <jlanloze@gmail.com>
2023-07-30 22:57:02 +0200

    commit

---
+++ 8kyu/You_only_need_one.js
@@ -0,0 +1,23 @@
+/* You will be given an array a and a value x. All you need to do is check whether the provided array contains the value.
+
+Array can contain numbers or strings. X can be either.
+
+Return true if the array contains the value, false if not.
+
+ */
+
+// My solution:
+
+funnction check(a, x) {
+    return a. includes(x);
+}
+
+// Other solutions:
+
+const check = (a,x)=>a.includes(x);
+
+// Another one:
+
+function check(a,x){
+    return a.indexOf(x) > -1;
+  };
