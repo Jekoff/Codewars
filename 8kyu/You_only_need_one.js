bfcaa23a03acada4a0e0d108f4ed033efaf3efcc
Jekoff <jlanloze@gmail.com>
2023-07-31 21:37:05 +0200

    commit

--- 8kyu/You_only_need_one.js
+++ 8kyu/You_only_need_one.js
@@ -9,7 +9,7 @@ Return true if the array contains the value, false if not.
 // My solution:
 
 funnction check(a, x) {
-    return a. includes(x);
+    return a.includes(x);
 }
 
 // Other solutions:
