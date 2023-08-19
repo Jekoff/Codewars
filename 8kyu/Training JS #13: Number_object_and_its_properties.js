d093f31c5473629b8155b7737da41c7a0275a7b1
Jekoff <jlanloze@gmail.com>
2023-08-19 21:08:48 +0200

    commit

--- 8kyu/Training JS #13: Number_object_and_its_properties.js
+++ 8kyu/Training JS #13: Number_object_and_its_properties.js
@@ -20,22 +20,23 @@ whatNumberIsIt(100) should return "Input number is 100"
 
 // My Solution:
 
-
-function whatNumberIsIt(n) {
-    if (Number.isNaN(n)) {
-        return "Input number is Number.NaN";
-    } else if (n === Number.POSITIVE_INFINITY) {
-        return "Input number is Number.POSITIVE_INFINITY";
-    } else if (n === Number.NEGATIVE_INFINITY) {
-        return "Input number is Number.NEGATIVE_INFINITY";
-    } else if (n === Number.MAX_VALUE) {
-        return "Input number is Number.MAX_VALUE";
+function whatNumberIsIt(n){
+    if (n === Number.MAX_VALUE) {
+        return "Input number is Number.MAX_VALUE"
     } else if (n === Number.MIN_VALUE) {
-        return "Input number is Number.MIN_VALUE";
+        return "Input number is Number.MIN_VALUE"
+    } else if (n === Number.POSITIVE_INFINITY) {
+        return "Input number is Number.POSITIVE_INFINITY"
+    } else if (n === Number.NEGATIVE_INFINITY) {
+        return "Input number is Number.NEGATIVE_INFINITY"
+    } else if (isNaN(n)) {
+        return "Input number is Number.NaN"
     } else {
-        return "Input number is " + n;
+        return "Input number is " + n
     }
-}
+
+
+
 
 console.log(whatNumberIsIt(1/0)); // Output: "Input number is Number.POSITIVE_INFINITY"
 console.log(whatNumberIsIt(100));  // Output: "Input number is 100"
