a3e57a03a05e5faa69c4c4392d5287435e498caa
Jekoff <jlanloze@gmail.com>
2023-08-28 09:47:58 +0200

    commit

--- 8kyu/Training JS #02: Basic_datatypes_Number.js
+++ 8kyu/Training JS #02: Basic_datatypes_Number.js
@@ -2,8 +2,8 @@
 defines six global variables v1 v2 v3 v4 v5 v6, every function has 
 two local variables a,b, please set the appropriate value for the two 
 variables(select from v1--v6), making these function return value equal 
-to 100. the function equal1 is completed, please refer to this example t
-o complete the following functions.
+to 100. the function equal1 is completed, please refer to this example to 
+complete the following functions.
 
 When you have finished the work, click "Run Tests" to see if your code is
  working properly.
@@ -34,3 +34,10 @@ function equal5() {
     let a=v2, b=v4;
     return a%b;
 }
+var v1=50; v2=100, v3=150, v4=200, v5=2, v6=250
+
+const equal1 = () => v1 + v1
+const equal2 = () => v3 - v1
+const equal3 = () => v1 * v5
+const equal4 = () => v4 / v5
+const equal5 = () => v2 % v4
