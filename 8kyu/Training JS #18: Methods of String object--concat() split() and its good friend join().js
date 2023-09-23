85756ab6959afaf3d8ec2134285a44e896e7f4f2
Jekoff <jlanloze@gmail.com>
2023-09-24 01:47:26 +0200

    commmit

---
+++ 8kyu/Training JS #18: Methods of String object--concat() split() and its good friend join().js
@@ -0,0 +1,30 @@
+/*
+
+Implement a function which accepts 2 arguments: string and separator.
+
+The expected algorithm: split the string into words by spaces, split each word into separate characters and join them back with the specified separator, join all the resulting "words" back into a sentence with spaces.
+
+For example:
+
+splitAndMerge("My name is John", " ")  ==  "M y n a m e i s J o h n"
+splitAndMerge("My name is John", "-")  ==  "M-y n-a-m-e i-s J-o-h-n"
+splitAndMerge("Hello World!", ".")     ==  "H.e.l.l.o W.o.r.l.d.!"
+splitAndMerge("Hello World!", ",")     ==  "H,e,l,l,o W,o,r,l,d,!"
+
+*/ 
+
+// My Solution:
+function splitAndMerge(str,sp){
+    return str.split(' ').map(word => word.split('').join(sp)).join(' ');
+}
+
+// Other Solutions:
+var str='My name is John';
+var sp='';
+var arr=str.split('');
+var arr2=[];
+for (var i=0;i<arr.lenght;i++){
+    if (arr[i]!==''){
+        arr2.push(arr[i]);
+    }
+}
