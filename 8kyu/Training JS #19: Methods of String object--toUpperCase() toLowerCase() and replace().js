1604a6b4c1616f949814cbc63d5f527387f61729
Jekoff <jlanloze@gmail.com>
2023-10-03 23:50:37 +0200

    commit

--- 8kyu/Training JS #19: Methods of String object--toUpperCase() toLowerCase() and replace().js
+++ 8kyu/Training JS #19: Methods of String object--toUpperCase() toLowerCase() and replace().js
@@ -22,3 +22,7 @@ function alienLanguage(str) {
     return str.split('').map(word => word.slice(0, -1).toUpperCase() + word.slice(-1).toLowerCase())
     .join('');
 }
+
+// Other Solutions:
+
+const alienLanguage = str => str.toUpperCase().replace(/\w\b/g,m => m.toLowerCase())
